db11062a6bee6b21372c2e103e418be77051c327
David Mytton <david@davidmytton.co.uk>
2021-01-02 17:23:08 +0000

    Set up vdirsyncer and khal for waybar

---
+++ waybar/waybar-khal.py
@@ -0,0 +1,38 @@
+# Based on https://gist.github.com/bjesus/178a9bd3453470d74803945dbbf9ed40
+#!/usr/bin/env python
+
+import subprocess
+import datetime
+import json
+from html import escape
+
+data = {}
+
+today = datetime.date.today().strftime("%Y-%m-%d")
+
+next_week = (datetime.date.today() +
+             datetime.timedelta(days=10)).strftime("%Y-%m-%d")
+
+output = subprocess.check_output("khal list "
+                                 "--format \"{start-style} {title}\" now "
+                                 + next_week,
+                                 shell=True)
+output = output.decode("utf-8")
+
+lines = output.split("\n")
+new_lines = []
+for line in lines:
+    clean_line = escape(line).split(" ::")[0]
+    if len(clean_line) and not clean_line[0] in ['0', '1', '2']:
+        clean_line = "\n<b>"+clean_line+"</b>"
+    new_lines.append(clean_line)
+output = "\n".join(new_lines).strip()
+
+if today in output:
+    data['text'] = "📅 " + output.split('\n')[1]
+else:
+    data['text'] = "📅 No events today"
+
+data['tooltip'] = output
+
+print(json.dumps(data))
